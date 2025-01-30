8e80d32a6b0ee3e5d1232ef77f7192cece2bdc85
Iqra-Nasir <128214218+Iqra-Nasir@users.noreply.github.com>
2025-01-30 05:55:36 +0500

    api-path

--- utils/url_config.js
+++ utils/url_config.js
@@ -1,2 +1,2 @@
-export const API_URL = "https://demo-milkywayitsolution.com/";
-// export const API_URL = "https://bttdc.nazmul.xyz";
+export const API_URL = "http://localhost:8000";
+// export const API_URL = "https://demo-milkywayitsolution.com/";
